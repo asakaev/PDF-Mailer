925604ffa31c52446799c0b26513bbec80785bba
Akhtyam Sakaev <ahtyam.s@livetex.ru>
2015-08-11 17:46:53 +0300

    Server handle GET request with name param and send email

--- server.js
+++ server.js
@@ -1,22 +1,66 @@
 var http = require('http');
 var moment = require('moment');
 var PDF = require('pdfkit');
+var nodemailer = require('nodemailer');
+var url = require('url');
+var queryString = require('querystring');
 
 var count = 0;
 
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: '',
+    pass: ''
+  }
+});
+
 http.createServer(function (req, res) {
+
+  // logs
   var ip = req.connection.remoteAddress;
   var time = '[' + moment().format('DD/MM/YY HH:MM:SS') + ']';
   var agent = req.headers["user-agent"];
   console.log((++count).toString(), ip, time, req.method, req.url, agent);
 
-  res.writeHead(200, {'Content-Type': 'application/pdf'});
+  var qs = url.parse(req.url).query;
+  var params = queryString.parse(qs);
 
-  var text = 'Hello world 2!';
+  // header
+  res.writeHead(200, {'Content-Type': 'plain/text'});
+
+  // PDF create
+  var name = params.name || 'username';
+  var text = 'When nine hundred years old you reach, look as good you' +
+      ' will not, ' + name + '.';
+
   var doc = new PDF();
-  doc.pipe(res);
   doc.text(text, 100, 100);
-  doc.end();
+  doc.end(); //we end the document writing.
 
-//  res.end('Hello World\n');
+  var mailOptions = {
+    from: 'Mail Tester <email@email.com>',
+    to: '', // list of receivers
+    subject: 'Test subject',
+    text: 'Hello, here will be mail body.', // plaintext body
+    attachments: [
+      {
+        filename: 'doc.pdf',
+        content: doc
+      }
+    ]
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    }
+
+    time = '[' + moment().format('DD/MM/YY HH:MM:SS') + ']';
+
+    console.log((count).toString(), ip, time, info.response);
+
+    return res.end('done!');
+  });
+
 }).listen(1337, '127.0.0.1');
