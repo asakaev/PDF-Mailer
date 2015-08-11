7d577ca38c395e92a4b84aeac5a00071ff1904db
Akhtyam Sakaev <ahtyam.s@livetex.ru>
2015-08-11 17:15:00 +0300

    PDF to email

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 var nodemailer = require('nodemailer');
+var PDF = require('pdfkit');
+
+var text = 'When nine hundred years old you reach, look as good you will not YO2!.';
+
+var doc = new PDF();
+doc.text(text, 100, 100);
+doc.end(); //we end the document writing.
 
 var transporter = nodemailer.createTransport({
   service: 'Gmail',
@@ -8,13 +15,17 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-
 var mailOptions = {
-  from: 'Fred Foo ✔ <foo@blurdybloop.com>',
+  from: 'Mail Tester <email@email.com>',
   to: 'email@email.com', // list of receivers
-  subject: 'Hello ✔',
-  text: 'Hello world ✔', // plaintext body
-  html: '<b>Hello world ✔</b> ' // html body
+  subject: 'Test subject',
+  text: 'Hello, here will be mail body.', // plaintext body
+  attachments: [
+    {
+      filename: 'doc.pdf',
+      content: doc
+    }
+  ]
 };
 
 transporter.sendMail(mailOptions, function(error, info) {
