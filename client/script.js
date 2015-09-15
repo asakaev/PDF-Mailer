3a60029baac94210807c9c132baea104bc3410ba
Akhtyam Sakaev <ahtyam.s@livetex.ru>
2015-09-15 18:59:49 +0300

    App scope, button handler

--- client/script.js
+++ client/script.js
@@ -1,45 +1,49 @@
-/**
- * @namespace
- */
-var pdfmailer = {};
+(function () {
+  /**
+   * @namespace
+   */
+  var pdfmailer = {};
 
 
-/**
- * Filter script tags
- *
- * @param {JQuery.node} html
- * @result {string}
- */
-pdfmailer.filter = function(html) {
-  html.find('script').remove();
-  html.find(':button').remove();
-  return html.html();
-};
+  /**
+   * Filter script tags
+   *
+   * @param {JQuery.node} html
+   * @result {string}
+   */
+  pdfmailer.filter = function(html) {
+    html.find('script').remove();
+    html.find(':button').remove();
+    return html.html();
+  };
 
 
-/**
- * Access point
- */
-pdfmailer.run = function() {
-  var bodyNode = $('body').clone();
-  var bodyHTML = pdfmailer.filter(bodyNode);
-  console.log(bodyHTML);
+  /**
+   * Access point
+   */
+  pdfmailer.run = function() {
+    var bodyNode = $('body').clone();
+    var bodyHTML = pdfmailer.filter(bodyNode);
+    console.log(bodyHTML);
 
-  var url = 'http://localhost:1337/';
+    var url = 'http://localhost:1337/';
 
-  function responseHandler(data) {
-    console.log(data);
-  }
+    function responseHandler(data) {
+      console.log(data);
+    }
 
-  $.ajax({
-    url: url,
-    type: 'POST',
-    beforeSend: function (xhr) {
-      xhr.setRequestHeader('Authorization', 'key_42');
-    },
-    data: bodyHTML,
-    contentType: 'application/x-www-form-urlencoded',
-    success: responseHandler,
-    error: responseHandler
-  });
-};
+    $.ajax({
+      url: url,
+      type: 'POST',
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader('Authorization', 'key_42');
+      },
+      data: bodyHTML,
+      contentType: 'application/x-www-form-urlencoded',
+      success: responseHandler,
+      error: responseHandler
+    });
+  };
+
+  $('.pdf_button').click(pdfmailer.run);
+}());
